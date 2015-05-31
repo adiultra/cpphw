90a64d405653850e9960b6687c809ff812939750
adiultra <adityaultra4@gmail.com>
2015-05-31 12:56:51 +0530

    removed bug in q1 c

--- q1/c/main.cpp
+++ q1/c/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     int p, r, t;
-    double si, ci;
+    double si, ci, x;
     cout << "Enter principle" << endl;
     cin >> p;
     cout << "Enter rate in %" << endl;
@@ -13,8 +13,9 @@ int main(){
     cout << "Enter Time" << endl;
     cin >> t;
     si = (p*r*t)/100;
-    cout << "The Simple Interest is " << si <<endl;
-    ci = p*pow((1+r/100),t);
-    cout << "The Compound Interest is " << ci << endl;
+    cout << "The Simple Interest is: " << si <<endl;
+    x = p*pow((1.0+r/100.0), t);
+    ci = x - p;
+    cout << "The Compound Interest is: " << ci << endl;
     return 0;
 }
