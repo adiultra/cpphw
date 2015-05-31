ed33315a397385871b64e7346ebe626618e9fad2
adiultra <adityaultra4@gmail.com>
2015-05-31 12:10:28 +0530

    did q1 b

---
+++ q1/b/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int a, yr, mnth, dys;
+    cout << "Enter the age in days" << endl;
+    cin >> a;
+    yr = a/365;
+    mnth = (a-yr*365)/30;
+    dys = a - (yr*365+mnth*30);
+    cout << "the Years are " << yr << endl;
+    cout << "the Months are " << mnth << endl;
+    cout << "the Days are " << dys << endl;
+    return 0;
+}
