0d6987440ecae07c74065d7772450289189bdf80
adiultra <adityaultra4@gmail.com>
2015-06-04 18:09:31 +0530

    did q1 e ;-) . .  |  -

---
+++ q1/e/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int a1, a2, h1, h2, kon;
+
+    cout << "Enter the age, height of student 1 (respectively)" << endl;
+    cin >> a1 >> h1;
+    cout << "Enter the age, height of student 2 (respectively)" << endl;
+    cin >> a2 >> h2;
+
+    kon = a1;
+    a1 = a2;
+    a2 = kon;
+
+    kon = h1;
+    h1 = h2;
+    h2 = kon;
+
+    cout << "The age, height of student 1 (respectively) is " << a1 << " " << h1 << endl;
+    cout << "The age, height of student 2 (respectively) is " << a2 << " " << h2 << endl;
+
+    return 0;
+}
